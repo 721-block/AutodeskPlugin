2a01340e04db261febb9ec0d911ef3e82e4be8ed
fearppen <72336814+fearppen@users.noreply.github.com>
2023-10-19 17:12:52 +0500

    Add files via upload

---
+++ RevitPlugin/RevitPlugin/RevitPlugin/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RevitPlugin
+{
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData externalCommandData, ref string message, ElementSet elements)
+        {
+            throw new System.Exception();
+        }
+    }
+}
