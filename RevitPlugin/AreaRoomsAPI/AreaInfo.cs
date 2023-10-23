62130a5a1bdc5c99569e43f30c8fdd6c39a7cbf5
S4MPAI <flekshum@gmail.com>
2023-10-23 18:24:22 +0500

    added AreaRoomsAPI

---
+++ RevitPlugin/AreaRoomsAPI/AreaInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRoomsAPI
+{
+    public class AreaInfo
+    {
+        public readonly IList<WallType> walls;
+
+        public AreaInfo(IList<WallType> walls)
+        {
+            this.walls = walls;
+        }
+
+        public int GetSquare()
+        {
+            return 0;
+        }
+    }
+}
---
+++ RevitPlugin/AreaRoomsAPI/Wall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AreaRoomsAPI
+{
+    public class Wall
+    {
+        public readonly Point startPoint;
+        public readonly Point endPoint;
+        public readonly WallType wallType;
+
+        public Wall(Point startPoint, Point endPoint, WallType wallType)
+        {
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+            this.wallType = wallType;
+        }
+    }
+}
