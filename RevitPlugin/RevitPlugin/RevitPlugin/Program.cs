76903274fc861a79353299cce7d5530818c8ee49
fearppen <antonchachylin@gmail.com>
2023-10-23 15:01:59 +0500

    add func

---
+++ RevitPlugin/RevitPlugin/RevitPlugin/Program.cs
@@ -0,0 +1,39 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Collections.Generic;
+
+namespace RevitPlugin
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Program : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData externalCommandData, ref string message, ElementSet elements)
+        {
+            var document = externalCommandData.Application.ActiveUIDocument;
+            var options = new Options();
+            var select = new List<Element>();
+
+            var appartmentReference = document.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Face);
+            var appartment = document.Document.GetElement(appartmentReference);
+            var apartmentGeometry = appartment.GetGeometryObjectFromReference(appartmentReference) as Face;
+            var ap = apartmentGeometry.GetEdgesAsCurveLoops();
+
+            var entranceReference = document.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Edge);
+            var entrance = document.Document.GetElement(entranceReference);
+            var entranceGeometry = entrance.GetGeometryObjectFromReference(entranceReference) as Edge;
+
+            var balconyReference = document.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Edge);
+            var balcony = document.Document.GetElement(balconyReference.ElementId);
+            var balconyGeometry = balcony.GetGeometryObjectFromReference(balconyReference) as Edge;
+            var edge1 = balconyGeometry.AsCurve();
+            var edge = balconyGeometry.AsCurve().GetEndPoint(0);
+            var x1 = balconyGeometry.AsCurve().GetEndPoint(1);
+
+            var roomsWindow = new Rooms(appartment, entrance, balcony);
+            roomsWindow.Show();
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ RevitPlugin/RevitPlugin/RevitPlugin/Rooms.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RevitPlugin
+{
+    /// <summary>
+    /// Логика взаимодействия для Rooms.xaml
+    /// </summary>
+    public partial class Rooms : Window
+    {
+        private Element pickedBox;
+        private Element entranceWall;
+        private Element balconyWall;
+
+        public Rooms(Element pickedBox, Element entranceWall, Element balconyWall)
+        {
+            InitializeComponent();
+            this.pickedBox = pickedBox;
+            this.entranceWall = entranceWall;
+            this.balconyWall = balconyWall;
+            var newList = new List<object> { pickedBox, entranceWall, balconyWall };
+            RoomsView.ItemsSource = newList;
+        }
+    }
+}
