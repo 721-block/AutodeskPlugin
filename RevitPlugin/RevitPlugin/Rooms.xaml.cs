510ae0f5b3717db22a74398cb11a593f9c7f1bf5
fearppen <antonchachylin@gmail.com>
2023-10-23 15:30:59 +0500

    fix project

---
+++ RevitPlugin/RevitPlugin/Rooms.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.DB;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace RevitPlugin
+{
+    /// <summary>
+    /// Логика взаимодействия для Rooms.xaml
+    /// </summary>
+    public partial class Rooms : Window
+    {
+        private Element pickedBox;
+        private Element entranceWall;
+        private Element balconyWall;
+
+        public Rooms(Element pickedBox, Element entranceWall, Element balconyWall)
+        {
+            InitializeComponent();
+            this.pickedBox = pickedBox;
+            this.entranceWall = entranceWall;
+            this.balconyWall = balconyWall;
+            var newList = new List<object> { pickedBox, entranceWall, balconyWall };
+            RoomsView.ItemsSource = newList;
+        }
+    }
+}
